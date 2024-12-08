358d91c347e4feab60983e01d51c2831063e4956
JoonasRajaniemi <156349310+JoonasRajaniemi@users.noreply.github.com>
2024-12-08 16:47:16 +0200

    Loput tiedostot
    Loput tiedostos

---
+++ obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("MyHybridApp.MainApplication, MyHybridApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64c234e9cd0445fd6e.MainApplication.class, crc64c234e9cd0445fd6e.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
